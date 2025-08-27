98b1ca2b714f931b13b67f7796242b948a276d53
lindsey5 <lindseysamson5@gmail.com>
2025-08-28 01:47:01 +0800

    initial commit

--- PMS-Frontend/vite.config.js
+++ PMS-Frontend/vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()], 
+  preview: {
+    host: true, // allows external access
+    port: process.env.PORT || 4173,
+    allowedHosts: ['projex-ph.onrender.com'] 
+  },
   define: {
     global: 'window',
   },
